991214c4bcaa4caf7884ebda0c9a2973cf51d85c
Cameron Smith <csmith@ucvts.tec.nj.us>
2019-03-26 10:42:57 -0400

    lol

--- trivia.js
+++ trivia.js
@@ -10,3 +10,27 @@ function baseballScore {
     bbScore = bbScore
   }
 }
+
+function basketballScore {
+  if /*The correct answer is clicked*/ {
+    bkScore++
+  } else /*The wrong answer is clicked*/ {
+    bkScore = bkScore
+  }
+}
+
+function baseballScore {
+  if /*The correct answer is clicked*/ {
+    bbScore++
+  } else /*The wrong answer is clicked*/ {
+    bbScore = bbScore
+  }
+}
+
+function baseballScore {
+  if /*The correct answer is clicked*/ {
+    bbScore++
+  } else /*The wrong answer is clicked*/ {
+    bbScore = bbScore
+  }
+}
