a6f7cb9f62b00f8a374d31a99837b0769af20fa4
Cameron Smith <csmith@ucvts.tec.nj.us>
2019-03-28 10:48:51 -0400

    ffs

--- trivia.js
+++ trivia.js
@@ -3,34 +3,34 @@ let bkScore;
 let fbScore;
 let tnScore;
 
-function baseballScore {
-  if /*The correct answer is clicked*/ {
-    bbScore++
-  } else /*The wrong answer is clicked*/ {
-    bbScore = bbScore
-  }
+function baseballScoreKeepCorrect {
+  bbScore = bbScore++
 }
 
-function basketballScore {
-  if /*The correct answer is clicked*/ {
-    bkScore++
-  } else /*The wrong answer is clicked*/ {
-    bkScore = bkScore
-  }
+function baseballScoreKeepWrong {
+  bbScore = bbScore
 }
 
-function footballScore {
-  if /*The correct answer is clicked*/ {
-    fbScore++
-  } else /*The wrong answer is clicked*/ {
-    fbScore = fbScore
-  }
+function basketballScoreKeepCorrect {
+  bkScore = bkScore++
 }
 
-function tennisScore {
-  if /*The correct answer is clicked*/ {
-    tnScore++
-  } else /*The wrong answer is clicked*/ {
-    tnScore = tnScore
-  }
+function basketballScoreKeepWrong {
+  bkScore = bkScore
+}
+
+function footballScoreKeepCorrect {
+  fbScore = fbScore++
+}
+
+function footballScoreKeepWrong {
+  fbScore = fbScore
+}
+
+function tennisScoreKeepCorrect {
+  tnScore = tnScore++
+}
+
+function tennisScoreKeepWrong {
+  tnScore = tnScore
 }
