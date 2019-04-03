39b8ef11e3414674c041a3a363d9a7fd8382b898
Cameron Smith <csmith@ucvts.tec.nj.us>
2019-04-03 09:44:15 -0400

    close

--- trivia.js
+++ trivia.js
@@ -1,41 +1,52 @@
-var bbScore;
-var bkScore;
-var fbScore;
-var tnScore;
+let bbScore;
+let bkScore;
+let fbScore;
+let tnScore;
 
 function baseballScoreKeepCorrect {
   bbScore = bbScore++
+  var div = document.getElementById("baseballScore");
+  div.innerHTML = "Final Score: " + bbScore + " / 5";
 }
 
 function baseballScoreKeepWrong {
   bbScore = bbScore
+  var div = document.getElementById("baseballScore");
+  div.innerHTML = "Final Score: " + bbScore + " / 5";
 }
 
 function basketballScoreKeepCorrect {
   bkScore = bkScore++
+  var div = document.getElementById("basketballScore");
+  div.innerHTML = "Final Score: " + bkScore + " / 5";
 }
 
 function basketballScoreKeepWrong {
   bkScore = bkScore
+  var div = document.getElementById("basketballScore");
+  div.innerHTML = "Final Score: " + bkScore + " / 5";
 }
 
 function footballScoreKeepCorrect {
   fbScore = fbScore++
+  var div = document.getElementById("footballScore");
+  div.innerHTML = "Final Score: " + fbScore + " / 5";
 }
 
 function footballScoreKeepWrong {
   fbScore = fbScore
+  var div = document.getElementById("footballScore");
+  div.innerHTML = "Final Score: " + fbScore + " / 5";
 }
 
 function tennisScoreKeepCorrect {
   tnScore = tnScore++
+  var div = document.getElementById("tennisScore");
+  div.innerHTML = "Final Score: " + tnScore + " / 5";
 }
 
 function tennisScoreKeepWrong {
   tnScore = tnScore
-}
-
-function findBBScore {
-  var p = document.getElementById("baseballScore");
-  p.innerHTML = "Final Score: " + bbScore + " / 5";
+  var div = document.getElementById("tennisScore");
+  div.innerHTML = "Final Score: " + tnScore + " / 5";
 }
